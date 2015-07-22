3b7032028c34a6bd37c81a8771d6daffef457318
The Good Maxwell <simonmaxwellraden@gmail.com>
2015-07-22 18:40:39 -0500

    Removed continue query, now stop on 0 power or 0 roll. Max Raden

--- src/Application/Guts.java
+++ src/Application/Guts.java
@@ -9,21 +9,19 @@ public class Guts {
 		double mod = input.nextDouble();
 		System.out.print("Enter Defense:");
 		double def = input.nextDouble();
-
-		String continueYN = "y";
-		while (continueYN.equalsIgnoreCase("y")) {
+		double power;
+		double roll;
+		do {
 			System.out.print("Enter Power:");
-			double power = input.nextDouble();
+			power = input.nextDouble();
 			System.out.print("Enter Roll:");
-			double roll = input.nextDouble();
+			roll = input.nextDouble();
 			double roll2 = roll / 10;
 			double power2 = power - def;
 			double mod2 = mod + roll2;
 			double damage = power2 * mod2;
 			System.out.println("Damage:" + damage);
-			System.out.print("Continue Y/N:");
-			continueYN = input.next();
-		}
+		} while (roll > 0 || power > 0);
 		input.close();
 	}
 }
