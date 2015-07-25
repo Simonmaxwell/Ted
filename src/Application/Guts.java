a90912647311b85697e843423cdd1995159eeec1
The Good Maxwell <simonmaxwellraden@gmail.com>
2015-07-25 11:48:53 -0500

    added functionalty to declare multiple enemies, and attack different targets.

--- src/Application/Guts.java
+++ src/Application/Guts.java
@@ -7,21 +7,32 @@ public class Guts {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter Modifier:");
 		double mod = input.nextDouble();
-		System.out.print("Enter Defense:");
-		double def = input.nextDouble();
+		double[] enemyArray = new double[7];
+		double def = 0;
+
+		for (int i = 1; def > -1; i++) {
+			System.out.print("Enter defense for Enemy" + i + ":");
+			def = input.nextDouble();
+			enemyArray[i] = def;
+		}
 		double power;
 		double roll;
+		int target = 0;
 		do {
+			double eDef = 0;
+			System.out.print("Enter target number:");
+			target = input.nextInt();
+			eDef = enemyArray[target];
 			System.out.print("Enter Power:");
 			power = input.nextDouble();
 			System.out.print("Enter Roll:");
 			roll = input.nextDouble();
 			double roll2 = roll / 10;
-			double power2 = power - def;
+			double power2 = power - eDef;
 			double mod2 = mod + roll2;
 			double damage = power2 * mod2;
 			System.out.println("Damage:" + damage);
-		} while (roll > 0 || power > 0);
+		} while (target > 0);
 		input.close();
 
 	}
